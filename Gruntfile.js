5914c28c7fe5a4ee0193609277e4bc7ade1a7e29
Ben Raziel <benraziel@gmail.com>
2014-07-19 22:35:49 +0300

    vulcanise ui components, put all the needed stuff in the "dist" directory

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,20 +4,31 @@ module.exports = function(grunt) {
     // Configure the copy task to move files from the development to production folders
     copy: {
       target: {
-        files: {
-  	  'src/js/gmod.js': '../gcode-modeler/gcode-modeler/build/emscripten/bin/gmod.js',
-  	  'dist/': ['src/**']
-        }
+        files: [
+  	  		{'dist/': ['parametric-ui.*', 'components/polymer/polymer.js', 'components/platform/platform.js']},
+        ]
       }
+    },
+    vulcanize: {
+    default: {
+      options: {
+        // Task-specific options go here.
+      },
+      files: {
+        'dist/components.html': 'components.html'
+      },
     }
+  },
   }); // initConfig end
 
   // Load plugins
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-vulcanize');
 
   // Run tasks
   grunt.registerTask('default', [
   				'clean',
-  				'copy']);
+  				'copy',
+          'vulcanize']);
 };
